19c47fe768ef9156fd2cba26bccdfab1d8aced83
Matthews A <matthewsabebe23@gmail.com>
2019-09-09 23:51:06 -0700

    psuedocode added and some basic layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+//This is for the display info
+//create 3 var one for answer,question, and choices
+//create a for loop having lenght = number of question
+//having each question showing in the html page.
+
+//this user input produces
+//create 4 var one for correct, incorrect, unanswer answer, and time
+//using if then statement to see if the user pick the correct answer
+// if correct then add one to correct var and run the next question
+//if incorrect then add one to incorrect var and run the next question
+//if user is out of time then add one to unanswer var and run the next question
+//once at the end of all the question exit loop
+//display number of correct/incorrect/unanswer question
+
+//reset function
+//if user click on reset button
+//set all var to 0 or empty
+//redisplay the first question
